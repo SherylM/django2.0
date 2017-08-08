0527cd939191c299093cc377ba7aacbce814c093
SherylM <sherylm2001@gmail.com>
2017-08-08 00:45:16 -0700

    Add files via upload
    THE SECOND TO FINAL VERSION 8/8/17

--- django/finalproject/sellbooks/buytextbooks/urls.py
+++ django/finalproject/sellbooks/buytextbooks/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = "books"
 
 urlpatterns = [
+
     #/buytextbooks/
-    url(r'^$', views.home, name='home'),#views.index,name = 'index'
+    url(r'^$', views.home, name='home'), #views.index,name = 'index'
 
     #/buytextbooks/detailtextbook.html/3(id or pk)/
     url(r'^detailtextbook.html/(?P<pk>[0-9]+)/$', views.TextbookDetailView.as_view(), name = 'detailtextbook'),
@@ -19,6 +20,29 @@ urlpatterns = [
     #/buytextbooks/prepbooks.html/
     url(r'^prepbooks.html/', views.PrepbookIndexView.as_view(), name='allprepbooks'),
 
+    #/buytextbooks/sell.html/
+    url(r'^sell.html/', views.sell, name='sell'),
+    
     #/buytextbooks/blog.html/
     url(r'^blog.html/', views.blog, name='blog'),
+
+    #/buytextbooks/signup.html/
+    url(r'^signup.html/', views.signup, name='signup'),
+
+    #/buytextbooks/login.html/
+    url(r'^login.html/', views.login, name='login'),
+
+    #/buytextbooks/textbook/add/
+    url(r'^textbook/add/$', views.TextbookCreate.as_view(), name = 'textbook-add'),
+
+    #/buytextbooks/prepbook/add/
+    url(r'^prepbook/add/$', views.PrepbookCreate.as_view(), name = 'prepbook-add'),
+
+    #/buytextbooks/textbook/3(id or pk)
+    url(r'^textbook/(?P<pk>[0-9]+)/$', views.TextbookUpdate.as_view(), name = 'textbook-update'),
+
+    #/buytextbooks/prepbook/3(id or pk)
+    url(r'^prepbook/(?P<pk>[0-9]+)/$', views.PrepbookUpdate.as_view(), name = 'prepbook-update'),
+    
 ]
+
